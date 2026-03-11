1d7fa7041335860002f748ae57aa39832597ad2c
Marcos <marcosjoaomj76@gmail.com>
2026-03-11 13:02:03 -0300

    Aulas JavaScript
    Aulas JavaScript do Curso em Video

---
+++ Desenvolvimento Web-Front/JavaScript/aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel= 59.5
+console.log(`a velocidade do seu carro é ${vel} khm/h`)
+if (vel >=60) { //condição simples
+    console.log('você ultrapassou a velocidade permitida.MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
+    
---
+++ Desenvolvimento Web-Front/JavaScript/testes/teste003/script.js
@@ -0,0 +1,14 @@
+var tx1 = document.getElementById('txt1')
+var bt1 = document.getElementById('btn1')
+var im1 = document.getElementById('img1')
+
+bt1.addEventListener(click, 'verificar')
+
+function verificar(){
+    im1.innerText = `Você tem ${tx1.value}.`
+
+    if (tx1<=60){
+        
+
+    }
+
