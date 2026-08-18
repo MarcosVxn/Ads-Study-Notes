a38526738b5da1cc634d95a458dd2a0bb916d91c
MarcosVxn <marcosjoaomj76@gmail.com>
2026-08-18 03:01:43 -0300

    feat: add environment configuration

--- Back-End/FastAPI/dependencies.py
+++ Back-End/FastAPI/dependencies.py
@@ -2,6 +2,13 @@ from models import db
 from sqlalchemy.orm import sessionmaker
 
 def pegar_sessao():
-    Session = sessionmaker(bind=db)
-    session = Session()
-    return session
+    # O try tenta executar o código dentro dele.
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+
+        # vai retornar a respota sem encerrar a execução da função
+        yield session
+    # O finally vai sempre ser executado indepentente da resposta que o try der
+    finally:
+        session.close
