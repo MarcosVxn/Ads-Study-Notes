a35cb7b4136debf2929cabfd278f8a425cc82d61
MarcosVxn <marcosjoaomj76@gmail.com>
2026-08-22 04:43:23 -0300

    feat: implement authentication and order routes

--- Back-End/FastAPI/auth_routes.py
+++ Back-End/FastAPI/auth_routes.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario, db
+from sqlalchemy.orm import Session
 from dependencies import pegar_sessao
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, LoginSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+# Token Temporario
+def criar_token(id_usuario):
+    token = f"XX(TW88LCof]g6h?S0uH2[KtJ{id_usuario}"
+    return token
+
 
 @auth_router.get("/")
 async def home():
@@ -26,3 +34,19 @@ async def criar_conta(usuarios_sechema: UsuarioSchema, session = Depends(pegar_s
         session.add(novo_usuario)
         session.commit()
         return {"Mensagem": f"Usuário cadastro com sucesso {usuarios_sechema.email}"}
+
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não encontrado")
+    else:
+        access_token = criar_token(usuario.id)
+        return{
+            "access_token": access_token,
+            "token_type": "Bearer"
+        }
+
+        # JWT Bearer
+
+        # headers = {"Access-Token": "Bearer token"}
