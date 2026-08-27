18a5681321ebac5c18d603fc6412da88ca81a11f
MarcosVxn <marcosjoaomj76@gmail.com>
2026-08-27 11:09:58 -0300

    feat(auth): substituir token provisório por geração de JWT real usando python-jose

--- Back-End/FastAPI/auth_routes.py
+++ Back-End/FastAPI/auth_routes.py
@@ -2,16 +2,43 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario, db
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
+from jose import jwt, JWTError
+from datetime import datetime,timedelta,timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 # Token Temporario
-def criar_token(id_usuario):
-    token = f"XX(TW88LCof]g6h?S0uH2[KtJ{id_usuario}"
-    return token
+def criar_token(id_usuario, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+
+    #esse datetime.now serve para pegar a hora que o usuario entrar e usar de acordo com o timezone UTC que é a hora padrão e o timedelta é oque importamos da main
+
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    # Verificar se o token é valido
+    #extrarir o id do usuario do token
+    usuario = session.query(Usuario).filter(Usuario.id==1).firts()
+    return
+
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if not usuario:
+        return False
+    
+    # no bcrypt utilizamos o verify uma função do bcrypt que verifica se a hash é a mesma que a senha do usuario como passamos no parametro
+
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+
+    # caso a função percorrer todo o if not e elif not quer dizer que deu TRUE então retornarar usuario
+
+    return usuario
 
 
 @auth_router.get("/")
@@ -37,16 +64,32 @@ async def criar_conta(usuarios_sechema: UsuarioSchema, session = Depends(pegar_s
 
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
-        raise HTTPException(status_code=400, detail="Usuário não encontrado")
+        raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais invalida")
     else:
         access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return{
             "access_token": access_token,
+            "refrash_token": refresh_token,
             "token_type": "Bearer"
         }
 
         # JWT Bearer
 
         # headers = {"Access-Token": "Bearer token"}
+
+
+@auth_router.get("/refresh")
+async def use_refresh_token(token):
+
+    # verificação do token
+
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return{
+        "access_token": access_token,
+        "token_type": "Bearer"
+    }
+    
